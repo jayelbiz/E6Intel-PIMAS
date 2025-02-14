e6b6f5cdb6eb61fbed828fe52fdfc185357b79b8
Jason Elliott <jasonethedesigner@gmail.com>
2025-02-13 21:40:04 -0500

    app update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 // Import routes
 import { publicRoutes, protectedRoutes } from './routes/route';
@@ -24,53 +24,51 @@ import './layouts/styles.scss';
 
 const App = () => {
   return (
-    <Router>
-      <AuthProvider>
-        <Suspense fallback={
-          <div className="auth-page-wrapper pt-5">
-            <div className="auth-page-content">
-              <div className="container">
-                <div className="row justify-content-center">
-                  <div className="col-md-8 col-lg-6 col-xl-5 text-center">
-                    <i className="pi pi-spin pi-spinner" style={{ fontSize: '3rem' }}></i>
-                  </div>
+    <AuthProvider>
+      <Suspense fallback={
+        <div className="auth-page-wrapper pt-5">
+          <div className="auth-page-content">
+            <div className="container">
+              <div className="row justify-content-center">
+                <div className="col-md-8 col-lg-6 col-xl-5 text-center">
+                  <i className="pi pi-spin pi-spinner" style={{ fontSize: '3rem' }}></i>
                 </div>
               </div>
             </div>
           </div>
-        }>
-          <Routes>
-            {/* Public Routes */}
-            {publicRoutes.map((route, idx) => (
-              <Route
-                key={idx}
-                path={route.path}
-                element={
-                  <NonAuthLayout>
-                    <route.component />
-                  </NonAuthLayout>
-                }
-              />
-            ))}
+        </div>
+      }>
+        <Routes>
+          {/* Public Routes */}
+          {publicRoutes.map((route, idx) => (
+            <Route
+              key={idx}
+              path={route.path}
+              element={
+                <NonAuthLayout>
+                  <route.component />
+                </NonAuthLayout>
+              }
+            />
+          ))}
 
-            {/* Protected Routes */}
-            {protectedRoutes.map((route, idx) => (
-              <Route
-                key={idx}
-                path={route.path}
-                element={
-                  <Authmiddleware>
-                    <VerticalLayout>
-                      <route.component />
-                    </VerticalLayout>
-                  </Authmiddleware>
-                }
-              />
-            ))}
-          </Routes>
-        </Suspense>
-      </AuthProvider>
-    </Router>
+          {/* Protected Routes */}
+          {protectedRoutes.map((route, idx) => (
+            <Route
+              key={idx}
+              path={route.path}
+              element={
+                <Authmiddleware>
+                  <VerticalLayout>
+                    <route.component />
+                  </VerticalLayout>
+                </Authmiddleware>
+              }
+            />
+          ))}
+        </Routes>
+      </Suspense>
+    </AuthProvider>
   );
 };
 
