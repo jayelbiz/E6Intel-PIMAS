7c545e83a419f411cf4e0f0bf165e65eb258c530
Jason Elliott <jasonethedesigner@gmail.com>
2025-02-16 07:57:12 -0500

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -21,55 +21,55 @@ import './assets/scss/theme.scss';
 import './layouts/styles.scss';
 
 const App = () => {
-  return (
-    <Router>
-      <AuthProvider>
-        <NewsProvider>
-          <Suspense fallback={
-            <div className="auth-page-wrapper pt-5">
-              <div className="auth-page-content">
-                <div className="container">
-                  <div className="row justify-content-center">
-                    <div className="col-md-8 col-lg-6 col-xl-5 text-center">
-                      <i className="pi pi-spin pi-spinner" style={{ fontSize: '3rem' }}></i>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          }>
-            <Routes>
-              {/* Public Routes */}
-              {publicRoutes.map((route, idx) => (
-                <Route
-                  key={idx}
-                  path={route.path}
-                  element={
-                    <NonAuthLayout>
-                      <route.component />
-                    </NonAuthLayout>
-                  }
-                />
-              ))}
+    return (
+        <Router>
+            <AuthProvider>
+                <NewsProvider>
+                    <Suspense fallback={
+                        <div className="auth-page-wrapper pt-5">
+                            <div className="auth-page-content">
+                                <div className="container">
+                                    <div className="row justify-content-center">
+                                        <div className="col-md-8 col-lg-6 col-xl-5 text-center">
+                                            <i className="pi pi-spin pi-spinner" style={{ fontSize: '3rem' }}></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    }>
+                        <Routes>
+                            {/* Public Routes */}
+                            {publicRoutes.map((route, idx) => (
+                                <Route
+                                    key={idx}
+                                    path={route.path}
+                                    element={
+                                        <NonAuthLayout>
+                                            <route.component />
+                                        </NonAuthLayout>
+                                    }
+                                />
+                            ))}
 
-              {/* Protected Routes */}
-              {protectedRoutes.map((route, idx) => (
-                <Route
-                  key={idx}
-                  path={route.path}
-                  element={
-                    <VerticalLayout>
-                      <route.component />
-                    </VerticalLayout>
-                  }
-                />
-              ))}
-            </Routes>
-          </Suspense>
-        </NewsProvider>
-      </AuthProvider>
-    </Router>
-  );
+                            {/* Protected Routes */}
+                            {protectedRoutes.map((route, idx) => (
+                                <Route
+                                    key={idx}
+                                    path={route.path}
+                                    element={
+                                        <VerticalLayout>
+                                            <route.component />
+                                        </VerticalLayout>
+                                    }
+                                />
+                            ))}
+                        </Routes>
+                    </Suspense>
+                </NewsProvider>
+            </AuthProvider>
+        </Router>
+    );
 };
 
 export default App;
---
+++ src/routes/route.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+// Public pages
+const Login = React.lazy(() => import('../pages/Authentication/Login'));
+const Register = React.lazy(() => import('../pages/Authentication/Register'));
+const ForgetPassword = React.lazy(() => import('../pages/Authentication/ForgetPassword'));
+
+// Private pages
+const Dashboard = React.lazy(() => import('../pages/Dashboard'));
+const News = React.lazy(() => import('../pages/News'));
+const Map = React.lazy(() => import('../pages/Map'));
+const Analysis = React.lazy(() => import('../pages/Analysis'));
+const Profile = React.lazy(() => import('../pages/Profile'));
+const Settings = React.lazy(() => import('../pages/Settings'));
+
+const publicRoutes = [
+    { path: '/login', component: Login },
+    { path: '/register', component: Register },
+    { path: '/forgot-password', component: ForgetPassword },
+    { path: '*', component: () => <Navigate to="/login" /> }
+];
+
+const protectedRoutes = [
+    { path: '/', component: Dashboard },
+    { path: '/news', component: News },
+    { path: '/news/:id', component: News },
+    { path: '/map', component: Map },
+    { path: '/analysis', component: Analysis },
+    { path: '/profile', component: Profile },
+    { path: '/settings', component: Settings }
+];
+
+export { publicRoutes, protectedRoutes };
