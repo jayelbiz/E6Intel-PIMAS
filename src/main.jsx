d4d969b2a428f4aefc89544eb7ec50b9633de504
Jason Elliott <jasonethedesigner@gmail.com>
2025-02-16 08:50:42 -0500

    verify imports

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import 'primeicons/primeicons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/scss/theme.scss';
 import './styles/animations.css';
+import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
